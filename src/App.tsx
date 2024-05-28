5c459d459404caa36e516f0a4941de0eed0ccf6e
rickhy siswanto <rickhysiswanto@gmail.com>
2024-05-28 08:45:39 +0700

    feat: toast css

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { Web3ModalProvider } from "./providers/web3modal"
 import Index from "./views/Index"
 import "./index.scss"
 import { DarkModeProvider } from "./context/DarkModeContext"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
     <Web3ModalProvider>
       <DarkModeProvider>
+        <ToastContainer position="bottom-left" theme="colored" />
         <Index />
       </DarkModeProvider>
     </Web3ModalProvider>
